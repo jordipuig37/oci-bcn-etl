1e11c5214cbc9c30c54fbcf293b437226317bbe0
jordipg37 <jordipuig37@gmail.com>
2023-05-21 13:47:17 +0200

    Modularized query data and added tests
    - The part where the API is accessed is a single function
    - Three data quality tests for the source are provided

--- src/download_json.py
+++ src/download_json.py
@@ -6,16 +6,24 @@ from types import SimpleNamespace
 
 from . import utils
 
-def main(args: SimpleNamespace):
+
+def query(args: SimpleNamespace) -> dict:
+    """Queries the API to get Barcelona Oci data in json format."""
     base_url = "https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search"
     parameters = {
         "resource_id": "877ccf66-9106-4ae2-be51-95a9f6469e4c"
     }
-    os.makedirs(args.data_root, exist_ok=True)
-    json_filename = os.path.join(args.data_root, utils.generate_daily_filename())
     resp = requests.get(base_url, params=parameters)
+
+    return resp.json()
+
+
+def main(args: SimpleNamespace):
+    data = query(args)
+    json_filename = os.path.join(args.data_root, utils.generate_daily_filename())
+    os.makedirs(args.data_root, exist_ok=True)
     with open(json_filename, "w") as jf:
-        json.dump(resp.json(), jf)
+        json.dump(data, jf)
 
 
 if __name__ == "__main__":
---
+++ tests/test_data_quality.py
@@ -0,0 +1,45 @@
+import pytest
+import pandas as pd
+from types import SimpleNamespace
+
+import src.utils as utils
+from src.download_json import query
+
+
+@pytest.fixture
+def args() -> SimpleNamespace:
+    return utils.args_from_config("config.ini")
+
+@pytest.fixture
+def src_json_data(args) -> dict:
+    return query(args)
+
+@pytest.fixture
+def data_columns() -> list[str]:
+    return ['_id', 'register_id', 'name', 'institution_id', 'institution_name',
+     'created', 'modified', 'addresses_roadtype_id', 'addresses_roadtype_name',
+     'addresses_road_id', 'addresses_road_name', 'addresses_start_street_number',
+     'addresses_end_street_number', 'addresses_neighborhood_id',
+     'addresses_neighborhood_name', 'addresses_district_id',
+     'addresses_district_name', 'addresses_zip_code', 'addresses_town',
+     'addresses_main_address', 'addresses_type', 'values_id',
+     'values_attribute_id', 'values_category', 'values_attribute_name',
+     'values_value', 'values_outstanding', 'values_description',
+     'secondary_filters_id', 'secondary_filters_name',
+     'secondary_filters_fullpath', 'secondary_filters_tree',
+     'secondary_filters_asia_id', 'geo_epgs_25831_x',
+     'geo_epgs_25831_y', 'geo_epgs_4326_x', 'geo_epgs_4326_y']
+
+
+def test_source_result(src_json_data: dict):
+    assert "result" in src_json_data
+
+
+def test_source_records(src_json_data: dict):
+    assert "records" in src_json_data["result"]
+
+
+def test_source_columns(src_json_data: dict, data_columns: list[str]):
+    for column_name in data_columns:
+        assert column_name in src_json_data["result"]["records"][0], \
+            f"Column {column_name} not in source data"

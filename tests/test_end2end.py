e42e96cf6ce53b65f2bdd41a735f5436aa97f73a
jordipg37 <jordipuig37@gmail.com>
2023-06-04 12:32:22 +0200

    Added end2end testing

---
+++ tests/test_end2end.py
@@ -0,0 +1,20 @@
+import pytest
+
+import src.utils as utils
+from src.download_json import main as download_json
+from src.to_sqlite import main as to_sqlite
+
+
+
+@pytest.fixture
+def args():
+    return utils.args_from_config("config.ini")
+
+
+def test_full_pipeline(args):
+    try:
+        download_json(args)
+        to_sqlite(args)
+        assert True
+    except BaseException as e:
+        assert False, f"Pipeline execution failed: {str(e)}"
